5c3fa5e5acf48c77b7b25a597ba647b1bd4a2867
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2026-06-06 13:02:43 -0400

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://poisnoir.github.io',
+  url: 'https://docs.precicore.ca',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/precicore-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
