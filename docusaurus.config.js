235e31d8a0f02a04a970dc2dbe84109a772dc1dd
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2026-06-06 13:08:05 -0400

    remove edit link and pagination

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,10 +47,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: undefined,
+          showLastUpdateTime: false,
+          pagination: false,
         },
         blog: false,
         theme: {
